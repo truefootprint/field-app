6c32713d934299ebd7747722e408778c7757b364
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-25 15:14:13 +0000

    Make innerText cope with more cases
    Specifically, cope with the case where innerText
    is called on render(<Foo />) directly rather than
    the result of a query.

--- spec/helpers/inner_text.js
+++ spec/helpers/inner_text.js
@@ -2,10 +2,13 @@ const innerText = (node) => {
   if (typeof node === "string") return node;
   if (typeof node === "number") return `${node}`;
 
-  if (node.props) {
-    const children = [node.props.children].flat();
+  if (node.children) {
+    const children = [node.children].flat();
     return children.map(c => innerText(c)).join("");
   }
+
+  if (node.getByTestId) return innerText(node.toJSON());
+  if (node.props) return innerText(node.props);
 
   return "";
 };
