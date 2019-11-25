471a1ceaaae7e0b8a9737c285c8b511d6f157945
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-25 16:48:27 +0000

    Add Carly’s color palette

---
+++ app/components/layout/palette.js
@@ -0,0 +1,49 @@
+const palette = {
+  green: {
+    primary:       "rgb(108, 187, 154)",
+    primaryTint:   "rgb(103, 177, 146)",
+    secondary:     "rgb(95, 115, 106)",
+    secondaryTint: "rgb(75, 87, 81)",
+  },
+
+  yellow: {
+    primary:       "rgb(211, 191, 115)",
+    primaryTint:   "rgb(198, 180, 110)",
+    secondary:     "rgb(159, 155, 105)",
+    secondaryTint: "rgb(110, 107, 71)",
+  },
+
+  blue: {
+    primary:       "rgb(70, 137, 168)",
+    primaryTint:   "rgb(63, 129, 160)",
+    secondary:     "rgb(77, 104, 117)",
+    secondaryTint: "rgb(53, 76, 86)",
+  },
+
+  red: {
+    primary:       "rgb(187, 107, 115)",
+    primaryTint:   "rgb(171, 104, 111)",
+    secondary:     "rgb(133, 91, 101)",
+    secondaryTint: "rgb(108, 69, 79)",
+  },
+
+  purple: {
+    primary:       "rgb(112, 99, 142)",
+    primaryTint:   "rgb(103, 91, 129)",
+    secondary:     "rgb(78, 64, 83)",
+    secondaryTint: "rgb(56, 42, 61)",
+  },
+
+  black: {
+    primary:       "rgb(51, 51, 51)",
+    primaryTint:   "rgb(17, 17, 17)",
+  },
+
+  white: {
+    primary:       "rgb(238, 238, 237)",
+    primaryTint:   "rgb(229, 229, 224)",
+    primaryTint2:  "rgb(213, 213, 206)",
+  }
+};
+
+export default palette;
