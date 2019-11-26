af565a1ea6e1cd73c83fa6c5ba61805d1d471d29
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-26 12:54:12 +0000

    Make spec helpers global for convenience

--- spec/helpers/have_text.js
+++ spec/helpers/have_text.js
@@ -1,14 +1,13 @@
 import innerText from "./inner_text";
-import expect from "expect";
 
-expect.extend({
-  toHaveText: (node, expected, { exact = false } = {}) => {
-    const actual = innerText(node);
-    const pass = exact ? actual === expected : new RegExp(expected).test(actual);
-    const word = exact ? "equal" : "match";
+const haveText = (node, expected, { exact = false } = {}) => {
+  const actual = innerText(node);
+  const pass = exact ? actual === expected : new RegExp(expected).test(actual);
+  const word = exact ? "equal" : "match";
 
-    return pass
-      ? { pass, message: () => `expected '${actual}' not to ${word} '${expected}'` }
-      : { pass, message: () => `expected '${actual}' to ${word} '${expected}'` };
-  },
-});
+  return pass
+    ? { pass, message: () => `expected '${actual}' not to ${word} '${expected}'` }
+    : { pass, message: () => `expected '${actual}' to ${word} '${expected}'` };
+};
+
+export default haveText;
--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,1 +1,14 @@
-import "./helpers/have_text";
+import { render, fireEvent } from "react-native-testing-library";
+import expect from "expect";
+import innerText from "./helpers/inner_text";
+import toHaveText from "./helpers/have_text";
+import props from "./helpers/props";
+import palette from "../app/components/layout/palette";
+
+global.render = render;
+global.fireEvent = fireEvent;
+global.innerText = innerText;
+global.props = props;
+global.palette = palette;
+
+expect.extend({ toHaveText });
