4f2af62a448e3677d4926cfaa09af98c45217e4e
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-27 17:32:32 +0000

    Add a more interesting Home example

--- app/screens/home.js
+++ app/screens/home.js
@@ -4,31 +4,38 @@ import Checkbox from "../components/checkbox";
 import Radio from "../components/radio";
 import RadioGroup from "../components/radio_group";
 
-const Home = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <View style={{ width: 50, height: 50 }} />
+const Home = ({ navigation }) => {
+  const options = ["Yes", "No", "Not sure"];
+  const [index, setIndex] = useState();
 
-    <Card color="blue" heading="Cement" number={1} outOf={3}>
-      <Text>Does the cement float in water?</Text>
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={{ width: 50, height: 50 }} />
 
-      <Submit color="blue" />
-    </Card>
+      <Card color="blue" heading="Cement" number={1} outOf={3}>
+        <Text>Does the cement float in water?</Text>
 
-    <Card color="green" heading="Sizes and position" number={2} outOf={3}>
-      <Text>Is the foundation going from gate to doorstep?</Text>
+        <Submit color="blue" />
+      </Card>
 
-      <RadioGroup color="green">
-        <Radio>Yes</Radio>
-        <Radio checked>No</Radio>
-        <Radio>Not sure</Radio>
-        <Checkbox>Report an issue</Checkbox>
-      </RadioGroup>
-    </Card>
+      <Card color="green" heading="Sizes and position" number={2} outOf={3}>
+        <Text>Is the foundation going from gate to doorstep?</Text>
 
-    <Card color="red" heading="Tiles" number={3} outOf={3}>
-      <Text>Are the tiles black and white?</Text>
-    </Card>
-  </View>
-);
+        <RadioGroup color="green" onChange={i => setIndex(i)}>
+          <Radio>Yes</Radio>
+          <Radio>No</Radio>
+          <Radio>Not sure</Radio>
+          <Checkbox onCheck={b => alert(`Issue: ${b}`)}>Report an issue</Checkbox>
+        </RadioGroup>
+
+        {typeof index === "undefined" ? null : <Text>Selected '{options[index]}'</Text>}
+      </Card>
+
+      <Card color="red" heading="Tiles" number={3} outOf={3}>
+        <Text>Are the tiles black and white?</Text>
+      </Card>
+    </View>
+  );
+};
 
 export default Home;
