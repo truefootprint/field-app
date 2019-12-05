e71170705c40bc3dc19c9cbefd561084eae6a389
Chris Patuzzo <chris@patuzzo.co.uk>
2019-12-05 10:39:10 +0000

    Rename spec/helpers -> spec/support

--- spec/globals.js
+++ spec/globals.js
@@ -1,10 +1,10 @@
 import "../app/globals";
 import { render, fireEvent } from "react-native-testing-library";
 import expect from "expect";
-import innerText from "./helpers/inner_text";
-import toHaveText from "./helpers/have_text";
-import props from "./helpers/props";
-import style from "./helpers/style";
+import innerText from "./support/inner_text";
+import toHaveText from "./support/have_text";
+import props from "./support/props";
+import style from "./support/style";
 
 global.render = render;
 global.fireEvent = fireEvent;
