dc7da2fa6aa6766aba85d5112f9cd12712f9a52d
Chris Patuzzo <chris@patuzzo.co.uk>
2019-12-09 15:19:40 +0000

    Add a script for benchmarking
    This is to keep an eye on database reset times.
    Initially, I plan to reset the database before
    each test for simplicity but if this starts to
    take a long time then a different approach is
    needed.

--- spec/globals.js
+++ spec/globals.js
@@ -1,6 +1,7 @@
 import "../app/globals";
 import { render, fireEvent } from "react-native-testing-library";
 import expect from "expect";
+import benchmark from "./support/benchmark";
 import innerText from "./support/inner_text";
 import toHaveText from "./support/have_text";
 import props from "./support/props";
@@ -8,6 +9,7 @@ import style from "./support/style";
 
 global.render = render;
 global.fireEvent = fireEvent;
+global.benchmark = benchmark;
 global.innerText = innerText;
 global.props = props;
 global.style = style;
---
+++ spec/support/benchmark.js
@@ -0,0 +1,14 @@
+const benchmark = async (callback, limit = 250) => {
+  const before = (new Date).getTime();
+  await callback();
+  const after = (new Date).getTime();
+  const duration = after - before;
+
+  if (duration > limit) {
+    console.warn(`Benchmark took ${duration}ms which exceeds ${limit}ms`);
+  }
+
+  return duration;
+};
+
+export default benchmark;
