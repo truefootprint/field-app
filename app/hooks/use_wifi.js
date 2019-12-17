ce4ba70d061da578ba3b442f6eaac06c65f2aac5
Chris Patuzzo <chris@patuzzo.co.uk>
2019-12-17 15:01:14 +0000

    Add a hook that triggers when a wifi connection is established

---
+++ app/hooks/use_wifi.js
@@ -0,0 +1,28 @@
+import NetInfo from "@react-native-community/netinfo";
+import * as Network from "expo-network";
+const wifi = Network.NetworkStateType.WIFI;
+
+// Calls the function when the internet becomes reachable via wifi.
+
+const useWifi = (onConnect) => {
+  const [connected, setConnected] = useState(false);
+
+  const handleChange = async (_net_info_state) => {
+    // NetInfo's state doesn't include isInternetReachable so use Network:
+    let state = {};
+    try { state = await Network.getNetworkStateAsync(); } catch {};
+
+    if (state.type === wifi && state.isInternetReachable) {
+      setConnected(true);
+      onConnect();
+    } else {
+      setConnected(false);
+    }
+  };
+
+  useEffect(() => NetInfo.addEventListener(handleChange), []);
+
+  return connected;
+};
+
+export default useWifi;
--- app/screens/home.js
+++ app/screens/home.js
@@ -2,13 +2,13 @@ import Layout from "../components/layout";
 import Project from "../components/project";
 import syncData from "../helpers/sync_data";
 import useForeground from "../hooks/use_foreground";
+import useWifi from "../hooks/use_wifi";
 
 const Home = ({ navigation }) => {
   const [data, setData] = useState();
 
-  useForeground(() => {
-    syncData(setData);
-  });
+  useWifi(() => { syncData(setData); });
+  useForeground(() => { syncData(setData); });
 
   if (!data) return null
 
