87e58085d315e0c652adb54d9d48aa02127cbdd4
Chris Patuzzo <chris@patuzzo.co.uk>
2020-01-01 17:20:22 +0000

    Add more SubmissionPeriod methods

--- app/helpers/submission_period.js
+++ app/helpers/submission_period.js
@@ -1,10 +1,20 @@
 const SubmissionPeriod = {};
+const duration = 24 * 60 * 60 * 1000;
 
-SubmissionPeriod.startedAt = () => {
+SubmissionPeriod.duration = duration;
+
+SubmissionPeriod.startedAt = (offset=0) => {
   const date = new Date();
   date.setHours(0, 0, 0, 0);
 
-  return date;
+  return new Date(date.getTime() + offset * duration);
+};
+
+SubmissionPeriod.endsAt = (offset=0) => {
+  const startedAt = SubmissionPeriod.startedAt();
+  const endTime = startedAt.getTime() + duration - 1;
+
+  return new Date(endTime + offset * duration);
 };
 
 SubmissionPeriod.secondsSinceStart = () => {
@@ -26,6 +36,28 @@ SubmissionPeriod.last = (collection) => {
   const objects = SubmissionPeriod.filter(collection);
 
   return maxBy(objects, o => new Date(o.createdAt));
+};
+
+SubmissionPeriod.offset = (date) => {
+  const startedAt = SubmissionPeriod.startedAt();
+  const sinceStart = new Date(date) - startedAt;
+  const periodsAgo = Math.floor(sinceStart / duration);
+
+  return periodsAgo;
+};
+
+SubmissionPeriod.partition = (collection, key="objects") => {
+  const groups = groupBy(collection, o => SubmissionPeriod.offset(o.createdAt));
+  const parts = [];
+
+  for (let [offset, objects] of Object.entries(groups)) {
+    const periodStart = SubmissionPeriod.startedAt(offset);
+    const periodEnd = SubmissionPeriod.endsAt(offset);
+
+    parts.push({ periodStart, periodEnd, [key]: objects });
+  }
+
+  return parts;
 };
 
 export default SubmissionPeriod;
--- spec/helpers/submission_period_spec.js
+++ spec/helpers/submission_period_spec.js
@@ -1,23 +1,48 @@
 import SubmissionPeriod from "../../app/helpers/submission_period";
 
 describe("SubmissionPeriod", () => {
-  const midnight = () => { const d = new Date(); d.setHours(0, 0, 0, 0); return d; };
+  const oneDay = SubmissionPeriod.duration;
+  const now = new Date();
+
+  const todayStart = (() => { const d = new Date(); d.setHours(0, 0, 0, 0); return d; })();
+  const yesterdayStart = new Date(todayStart.getTime() - oneDay);
+  const tomorrowStart = new Date(todayStart.getTime() + oneDay);
+
+  const todayEnd = new Date(todayStart.getTime() + oneDay - 1);
+  const yesterdayEnd = new Date(yesterdayStart.getTime() + oneDay - 1);
+  const tomorrowEnd = new Date(tomorrowStart.getTime() + oneDay - 1);
 
   it("starts from midnight of the current day", () => {
-    expect(SubmissionPeriod.startedAt()).toEqual(midnight());
+    expect(SubmissionPeriod.startedAt()).toEqual(todayStart);
   });
 
-  it("can return the number of seconds since midnight", () => {
-    const currentSeconds = new Date() / 1000;
-    const midnightSeconds = midnight() / 1000;
+  it("can calculate startedAt for a period in the past or future", () => {
+    expect(SubmissionPeriod.startedAt(-1)).toEqual(yesterdayStart);
+    expect(SubmissionPeriod.startedAt(1)).toEqual(tomorrowStart);
+  });
 
-    const delta = currentSeconds - midnightSeconds;
+  it("ends at just before midnight of the current day", () => {
+    expect(SubmissionPeriod.endsAt()).toEqual(todayEnd);
+  });
 
-    expect(SubmissionPeriod.secondsSinceStart()).toEqual(delta);
+  it("can calculate endsAt for a period in the past or future", () => {
+    expect(SubmissionPeriod.endsAt(-1)).toEqual(yesterdayEnd);
+    expect(SubmissionPeriod.endsAt(1)).toEqual(tomorrowEnd);
+  });
+
+  it("has a duration of one day", () => {
+    expect(SubmissionPeriod.duration).toEqual(24 * 60 * 60 * 1000);
+  });
+
+  it("can return the number of seconds since the start", () => {
+    const expected = new Date() / 1000 - todayStart / 1000;
+    const actual = SubmissionPeriod.secondsSinceStart();
+
+    expect(actual - expected).toBeLessThan(1);
   });
 
   it("can filter objects created within the submission period", () => {
-    const rightNow = { createdAt: new Date().toString() };
+    const rightNow = { createdAt: now.toString() };
     const agesAgo = { createdAt: new Date(0).toString() };
 
     const result = SubmissionPeriod.filter([rightNow, agesAgo]);
@@ -25,14 +50,40 @@ describe("SubmissionPeriod", () => {
   });
 
   it("can find the last object created within the submission period", () => {
-    const rightNow = { createdAt: new Date().toString() };
-    const earlier = { createdAt: midnight().toString() };
-    const agesAgo = { createdAt: new Date(0).toString() };
+    const rightNow = { createdAt: now.toString() };
+    const today = { createdAt: todayStart.toString() };
+    const yesterday = { createdAt: yesterdayStart.toString() };
 
-    const result = SubmissionPeriod.last([rightNow, earlier, agesAgo]);
+    const result = SubmissionPeriod.last([rightNow, today, yesterday]);
     expect(result).toEqual(rightNow);
 
-    const noResult = SubmissionPeriod.last([agesAgo]);
+    const noResult = SubmissionPeriod.last([yesterday]);
     expect(noResult).toBeUndefined();
+  });
+
+  it("can calculate the number of submission periods until the date", () => {
+    expect(SubmissionPeriod.offset(yesterdayStart)).toBe(-1);
+    expect(SubmissionPeriod.offset(yesterdayEnd)).toBe(-1);
+
+    expect(SubmissionPeriod.offset(todayStart)).toBe(0);
+    expect(SubmissionPeriod.offset(todayEnd)).toBe(0);
+
+    expect(SubmissionPeriod.offset(now)).toBe(0);
+
+    expect(SubmissionPeriod.offset(tomorrowStart)).toBe(1);
+    expect(SubmissionPeriod.offset(tomorrowEnd)).toBe(1);
+  });
+
+  it("partitions the objects by the submission period in which they were created", () => {
+    const rightNow = { createdAt: now.toString() };
+    const today = { createdAt: todayStart.toString() };
+    const yesterday = { createdAt: yesterdayStart.toString() };
+
+    const result = SubmissionPeriod.partition([rightNow, today, yesterday]);
+
+    expect(result).toEqual([
+      { periodStart: todayStart, periodEnd: todayEnd, objects: [rightNow, today] },
+      { periodStart: yesterdayStart, periodEnd: yesterdayEnd, objects: [yesterday] },
+    ]);
   });
 });
