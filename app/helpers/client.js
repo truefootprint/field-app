400ae3d2b16a5c9164f48b9662891a23c6fa1fd5
Chris Patuzzo <chris@patuzzo.co.uk>
2020-01-03 14:54:34 +0000

    Add a Client method to send updates to the backend

--- app/helpers/client.js
+++ app/helpers/client.js
@@ -1,5 +1,7 @@
 import basicAuth from "../../config/basic-auth.json";
 
+const host = "https://UPDATE_ME.ngrok.io";
+
 class Client {
   constructor() {
     // TODO credentials handshake
@@ -9,13 +11,30 @@ class Client {
     return this.getJSON("/my_data?user_name=Test&role_name=Test");
   }
 
+  myUpdates(updates) {
+    return this.postJSON("/my_updates?user_name=Test&role_name=Test", { updates });
+  }
+
   async getJSON(path) {
-    const host = "https://reporter-backend.truefootprint.com";
     const headers = { Authorization: `Basic ${basicAuth.base64}` };
     const response = await fetch(`${host}${path}`, { headers });
     const data = await response.json();
 
     return camelCaseKeys(data, { deep: true });
+  }
+
+  async postJSON(path, data) {
+    data = snakeCaseKeys(data, { deep: true });
+    data = JSON.stringify(data);
+
+    await fetch(`${host}${path}`, {
+      method: "POST",
+      headers: {
+        "Authorization": `Basic ${basicAuth.base64}`,
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
   }
 }
 
