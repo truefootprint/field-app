56a21a27a6584e794729942394c29252bd0ae9dd
Chris Patuzzo <chris@patuzzo.co.uk>
2020-01-06 11:51:37 +0000

    Don’t warn when presenting records that are pushed
    Otherwise Response.update({ pushed: true }, …)
    logs the warning, when we don’t actually use the
    return value and don’t care.

--- app/helpers/timestamp_field.js
+++ app/helpers/timestamp_field.js
@@ -2,8 +2,9 @@ const timestampField = (name) => ({
   type: Sequelize.DATE,
   get() {
     const date = this.getDataValue(name);
+    const pushed = this.getDataValue("pushed");
 
-    if (isNaN(date)) {
+    if (isNaN(date) && !pushed) {
       console.warn("Date is NaN. Set '{ raw: true }' to avoid a Sequelize bug.");
     }
 
