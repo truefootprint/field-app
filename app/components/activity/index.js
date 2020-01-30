491870ee754a81cc4df2cb0a9004b9026e041a19
Chris Patuzzo <chris@patuzzo.co.uk>
2020-01-30 14:04:27 +0000

    Auto-expand the current project activity

--- app/components/activity/index.js
+++ app/components/activity/index.js
@@ -1,14 +1,14 @@
 import Topic from "../topic";
 import Expander from "../expander";
 
-const Activity = ({ color="blue", name, projectQuestions={ byTopic: [] }, onAnswerQuestion=()=>{} }) => {
+const Activity = ({ color="blue", name, isCurrent=false, projectQuestions={ byTopic: [] }, onAnswerQuestion=()=>{} }) => {
   const counts = {};
 
   const topics = projectQuestions.byTopic.map(({ topic, projectQuestions }, i) => (
     <Topic key={i} color={color} questions={projectQuestions} onAnswerQuestion={onAnswerQuestion} {...topic} />
   ));
 
-  return Expander({ color, text: name, children: topics });
+  return Expander({ color, text: name, children: topics, expanded: isCurrent });
 };
 
 export default Activity;
--- app/components/project/index.js
+++ app/components/project/index.js
@@ -2,12 +2,12 @@ import Activity from "../activity";
 import Sticky from "../sticky";
 import Summary from "../summary";
 
-const Project = ({ name, projectSummary={}, projectActivities=[], onAnswerQuestion=()=>{} }) => (
+const Project = ({ name, projectSummary={}, projectActivities=[], currentProjectActivity={}, onAnswerQuestion=()=>{} }) => (
   <Sticky.Container>
     <Summary color="blue" name={name} text={projectSummary.text} activityCount={projectActivities.length} />
 
-    {projectActivities.map((props, i) => (
-      Activity({ color: palette.cycle(i), onAnswerQuestion, ...props })
+    {projectActivities.map(({ id, ...props }, i) => (
+      Activity({ color: palette.cycle(i), onAnswerQuestion, isCurrent: id === currentProjectActivity.id, ...props })
     ))}
   </Sticky.Container>
 );
