4f89b5cb89ccbd56f1404464a2e69b35f2fa625e
Chris Patuzzo <chris@patuzzo.co.uk>
2020-02-05 15:02:48 +0000

    Add a Downloader component

---
+++ app/components/downloader/index.js
@@ -0,0 +1,52 @@
+import downloadFile from "../../workflows/download_file";
+import Attachment from "../../models/attachment";
+import Button from "../button";
+import NoWifi from "../no_wifi";
+import styles from "./styles.js";
+
+const Downloader = ({ md5, children }) => {
+  const { connected } = useContext(AppContext);
+
+  const [downloaded, setDownloaded] = useState();
+  const [failed, setFailed] = useState();
+
+  const loadFile = async () => {
+    setFailed(false);
+
+    const attachment = await Attachment.findOne({ where: { md5 } });
+    const exists = await File.exists(attachment.filename);
+    if (exists) { setDownloaded(true); return; }
+
+    // We either need to cancel or wait for existing downloads to finish
+    // otherwise downloadFile returns false because there's one in progress.
+    if (Download.inProgress()) await Download.resume();
+
+    const success = await downloadFile(attachment.id);
+    if (success) { setDownloaded(true); return; }
+
+    setFailed(true);
+  };
+
+  useEffect(() => { loadFile().then(() => setFirstRun(false)); }, []);
+
+  const retryText = (
+    <View {...className("downloader", styles)}>
+      <Text>Download failed</Text>
+      <Button text="Retry" caps={false} fill={false} onPress={loadFile} />
+    </View>
+  );
+
+  const loadingText = (
+    <View {...className("downloader", styles)}>
+      <Text>Loading...</Text>
+    </View>
+  );
+
+  if (downloaded) return children;
+  if (!connected) return <NoWifi />;
+  if (failed) return retryText;
+
+  return loadingText;
+};
+
+export default Downloader;
---
+++ app/components/downloader/styles.js
@@ -0,0 +1,9 @@
+const styles = StyleSheet.create({
+  downloader: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default styles;
