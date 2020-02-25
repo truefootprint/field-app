1ae91f09189c9c87c4142a6878bd517e2557bec1
Chris Patuzzo <chris@patuzzo.co.uk>
2020-02-25 15:04:50 +0000

    Stitch versionedContent into existing issues and resolutions

--- app/helpers/stitch_my_data.js
+++ app/helpers/stitch_my_data.js
@@ -6,17 +6,37 @@ const stitchMyData = (myData, responses, contents) => {
     if (isProjectQuestion(o)) {
       const responses = responseGroups[o.id] || [];
       o.responses = o.responses.concat(responses);
+    }
 
-      const newIssues = [["ProjectQuestion", "Issue"], o.id]
-      const newResolutions = [["ProjectQuestion", "Resolution"], o.id]
+    if (isIssue(o)) {
+      const contents = contentGroups[["Issue", o.id]] || [];
+      o.versionedContent = newest(contents, o.versionedContent);
+    }
+
+    if (isResolution(o)) {
+      const contents = contentGroups[["Resolution", o.id]] || [];
+      o.versionedContent = newest(contents, o.versionedContent);
     }
 
     return o;
   });
 };
 
+const newest = (contents, existing) => {
+  if (existing) contents.push(existing);
+  return maxBy(contents, c => c.createdAt);
+};
+
 const isProjectQuestion = (o) => (
-  o && typeof o.id !== "undefined" && typeof o.responses !== "undefined"
+  o && typeof o.responses !== "undefined"
+);
+
+const isIssue = (o) => (
+  o && typeof o.versionedContent !== "undefined" && typeof o.issueId === "undefined"
+);
+
+const isResolution = (o) => (
+  o && typeof o.versionedContent !== "undefined" && typeof o.issueId !== "undefined"
 );
 
 export default stitchMyData;
--- spec/helpers/stitch_my_data_spec.js
+++ spec/helpers/stitch_my_data_spec.js
@@ -1,27 +1,71 @@
 import stitchMyData from "../../app/helpers/stitch_my_data";
 
 describe("stitchMyData", () => {
-  it("combines myData with additional responses from the user", () => {
-    const myData = { id: 123, responses: [] };
-    const responses = [{ projectQuestionId: 123, value: "answer" }];
-    const contents = [];
+  const now = new Date();
+  const hourAgo = new Date(now.getTime() - 60 * 60 * 1000);
 
-    const result = stitchMyData(myData, responses, contents);
-    expect(result).toEqual({ id: 123, responses });
+  it("combines myData with additional responses from the user", () => {
+    const myData = { id: 1, responses: [] };
+    const responses = [{ projectQuestionId: 1, value: "answer" }];
+    const result = stitchMyData(myData, responses, []);
+
+    expect(result).toEqual({ id: 1, responses });
   });
 
   it("handles arbitrary levels of nesting", () => {
     const myData = { a: { b: [{ c: { id: 123, responses: [] } }] } };
     const responses = [{ projectQuestionId: 123, value: "answer" }];
-    const contents = [];
+    const result = stitchMyData(myData, responses, []);
 
-    const result = stitchMyData(myData, responses, contents);
     expect(result).toEqual({ a: { b: [{ c: { id: 123, responses } }] } });
   });
 
+  it("overrides versionedContent for issues with content from the user", () => {
+    const myData = { issues: [{ id: 1, versionedContent: { text: "text" }}] };
+    const contents = [{ subjectType: "Issue", subjectId: 1, text: "new text" }];
+    const result = stitchMyData(myData, [], contents);
+
+    expect(result).toEqual({ issues: [{
+      id: 1,
+      versionedContent: {
+        subjectType: "Issue",
+        subjectId: 1,
+        text: "new text",
+      }
+    }]});
+  });
+
+  it("overrides versionedContent for resolutions with content from the user", () => {
+    const myData = { resolutions: [{ id: 1, issueId: 2, versionedContent: { text: "text" }}] };
+    const contents = [{ subjectType: "Resolution", subjectId: 1, text: "new text" }];
+    const result = stitchMyData(myData, [], contents);
+
+    expect(result).toEqual({ resolutions: [{
+      id: 1,
+      issueId: 2,
+      versionedContent: {
+        subjectType: "Resolution",
+        subjectId: 1,
+        text: "new text",
+      }
+    }]});
+  });
+
+  it("does not override versionedContent if the user's content is older", () => {
+    const myData = { issues: [{ id: 1, versionedContent: { text: "text", createdAt: now }}] };
+    const contents = [{ subjectType: "Issue", subjectId: 1, text: "new text", createdAt: hourAgo }];
+    const result = stitchMyData(myData, [], contents);
+
+    expect(result).toEqual({ issues: [{
+      id: 1,
+      versionedContent: {
+        text: "text", // has not updated
+        createdAt: now,
+      }
+    }]});
+  });
+
   // TODO:
-  //    - override versionedContent for Issue if newer in database
-  //    - override versionedContent for Resolution if newer in database
   //    - add to issues if composite ["...", "Issue"] key in database
   //    - add to resolutions if composite ["...", "Resolution"] key in database
 });
