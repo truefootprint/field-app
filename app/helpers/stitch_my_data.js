130fd711784e51540a03d76d7c44d426588b2132
Chris Patuzzo <chris@patuzzo.co.uk>
2020-03-11 12:38:09 +0000

    Stich issues and issue notes into myData

--- app/helpers/stitch_my_data.js
+++ app/helpers/stitch_my_data.js
@@ -1,18 +1,56 @@
-const stitchMyData = (myData, responses) => {
-  const responseGroups = groupBy(responses, r => r.projectQuestionId);
+const stitchMyData = (myData, responses, issueNotes) => {
+  const responsesByQuestion = groupBy(responses, r => r.projectQuestionId);
+  const notesBySubject = groupBy(issueNotes, n => [n.subjectType, n.subjectId]);
+  const notesByIssue = groupBy(issueNotes, n => n.issueUuid);
 
   return mapNested(myData, o => {
     if (isProjectQuestion(o)) {
-      const responses = responseGroups[o.id] || [];
+      const responses = responsesByQuestion[o.id] || [];
       o.responses = o.responses.concat(responses);
+
+      const issues = newIssues("ProjectQuestion", o.id, o.issues, notesBySubject);
+      o.issues = o.issues.concat(issues);
+    }
+
+    if (isIssue(o)) {
+      const notes = notesByIssue[o.uuid] || [];
+      o.notes = o.notes.concat(notes);
     }
 
     return o;
   });
 };
 
+const newIssues = (subjectType, subjectId, existingIssues, notesBySubject) => {
+  const notes = notesBySubject[[subjectType, subjectId]] || [];
+  const notesByIssue = groupBy(notes, n => n.issueUuid);
+
+  const existingUuids = new Set(existingIssues.map(i => i.uuid));
+  const newIssues = [];
+
+  for (const [uuid, notes] of Object.entries(notesByIssue)) {
+    if (existingUuids.has(uuid)) continue;
+
+    newIssues.push({
+      uuid,
+      subjectType,
+      subjectId,
+      createdAt: notes[0].createdAt,
+      updatedAt: notes[0].updatedAt,
+      resolved: notes.some(n => n.resolved),
+      notes: [],
+    });
+  }
+
+  return newIssues;
+};
+
 const isProjectQuestion = (o) => (
   o && typeof o.responses !== "undefined"
+);
+
+const isIssue = (o) => (
+  o && typeof o.resolved !== "undefined" && typeof o.uuid !== "undefined"
 );
 
 export default stitchMyData;
--- spec/helpers/stitch_my_data_spec.js
+++ spec/helpers/stitch_my_data_spec.js
@@ -12,6 +12,54 @@ describe("stitchMyData", () => {
     expect(result).toEqual({ id: 1, responses, issues: [] });
   });
 
+  it("combines myData with additional issue notes from the user", () => {
+    const myData = { uuid: "some-uuid", resolved: false, notes: [] };
+    const notes = [{ issueUuid: "some-uuid", text: "text" }];
+    const result = stitchMyData(myData, [], notes);
+
+    expect(result).toEqual({ uuid: "some-uuid", resolved: false, notes });
+  });
+
+  it("combines myData with additional issues from the user", () => {
+    const myData = { id: 1, responses: [], issues: [] };
+    const notes = [{
+      issueUuid: "some-uuid",
+      subjectType: "ProjectQuestion",
+      subjectId: 1,
+      text: "text",
+      resolved: true,
+      createdAt: hourAgo,
+      updatedAt: now,
+    }];
+
+    const result = stitchMyData(myData, [], notes);
+
+    expect(result).toEqual({
+      id: 1,
+      responses: [],
+      issues: [{
+        uuid: "some-uuid",
+        subjectType: "ProjectQuestion",
+        subjectId: 1,
+        resolved: true,
+        createdAt: hourAgo,
+        updatedAt: now,
+        notes: [{
+          issueUuid: "some-uuid",
+          text: "text",
+          resolved: true,
+          createdAt: hourAgo,
+          updatedAt: now,
+
+          // These fields are superfluous, they don't come from the backend but
+          // don't cause harm being on the issue note as well as the issue.
+          subjectType: "ProjectQuestion",
+          subjectId: 1,
+        }],
+      }]
+    });
+  });
+
   it("handles arbitrary levels of nesting", () => {
     const myData = { a: { b: [{ c: { id: 123, responses: [], issues: [] } }] } };
     const responses = [{ projectQuestionId: 123, value: "answer" }];
