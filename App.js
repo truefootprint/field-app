4422e9e462f215eee687e5885b6455ed0f08b78d
Chris Patuzzo <chris@patuzzo.co.uk>
2020-04-01 14:40:08 +0100

    Initialize Sentry before anything else

--- App.js
+++ App.js
@@ -1,1 +1,6 @@
+import initializeSentry from "./app/helpers/sentry";
+
+// Capture exceptions and send them to Sentry. Do this before loading anything.
+initializeSentry();
+
 export { default } from "./app/index";
