a1afc520cba0c2ebe5737a2802219de10fcb4ead
Chanda <mulaikopwapwa@gmail.com>
2020-07-13 16:11:14 +0200

    Enable mobile data usage

--- app/helpers/has_wifi.js
+++ app/helpers/has_wifi.js
@@ -1,11 +1,12 @@
 import * as Network from "expo-network";
 
 const wifi = Network.NetworkStateType.WIFI;
+const mobile = Network.NetworkStateType.CELLULAR;
 
 const hasWifi = async () => {
   try {
     const state = await Network.getNetworkStateAsync();
-    return state.type === wifi && state.isInternetReachable;
+    return (state.type === wifi || state.type === mobile) && state.isInternetReachable;
   } catch {
     return false;
   }
