c796f7e48b5808bb085fdd235b81d800aff71886
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2020-12-01 06:58:08 +0200

    Changes from expo upgrade

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import catchGlobalErrors from "./app/helpers/global_errors";
-import initializeSentry from "./app/helpers/sentry";
+//import catchGlobalErrors from "./app/helpers/global_errors";
+//import initializeSentry from "./app/helpers/sentry";
 
 // Capture exceptions and send them to Sentry. Do this before loading anything.
-initializeSentry();
+//initializeSentry();
 
 // Show a friendly message (instead of crashing) if something goes wrong.
-catchGlobalErrors();
+//catchGlobalErrors();
 
 export { default } from "./app/index";
 // deploy changes test Chanda
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,19 @@
-const blacklist = require("metro").createBlacklist;
+const blacklist = require('metro-config/src/defaults/blacklist'); //#.createBlacklist;
 
 module.exports = {
   resolver: {
     blacklistRE: blacklist([/spec\/.*/])
   }
 };
+
+
+//const blacklist = require('metro-config/src/defaults/blacklist');
+
+// blacklist is a function that takes an array of regexes and combines
+// them with the default blacklist to return a single regex.
+
+// module.exports = {
+//   resolver: {
+//     blacklistRE: blacklist([/dist\/.*/])
+//   }
+//};
