35c09011914d8876bf82239a857868945f2ba584
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2020-12-01 06:58:29 +0200

    Add custom project image for kenya demo

--- app/screens/home.js
+++ app/screens/home.js
@@ -14,6 +14,7 @@ const images = {
   6: require("../assets/images/project-id-6.jpg"),
   7: require("../assets/images/project-id-7.jpg"),
   8: require("../assets/images/project-id-8.jpg"),
+  239: require("../assets/images/project-id-239.png"),
   _: require("../assets/images/project-id-3.jpg"),
 };
 
@@ -30,10 +31,22 @@ const Home = ({ navigation }) => {
     }
   };
 
+  const selectProjectImage = (id, projectTypeId) => {
+    let result
+    if (projectTypeId === 6) {
+      result = images[7]
+    } else if (id === 239) {
+      result = images[239]
+    } else {
+      result = (images[id] || images._)
+    }
+    return result
+  }
+
   const projectCard = ({ name, id, projectTypeId }, i) => (
     <Card key={i} color={palette.cycle(i)} heading={name} number={i + 1} outOf={projects.length}>
       <TouchableOpacity {...className("touchable")} onPress={handlePress(i)} activeOpacity={0.8}>
-        <Image source={projectTypeId ===  6 ? images[7] : (images[id] || images._)} width={500} height={id === 8 ? 650 : 350} />
+        <Image source={selectProjectImage(id, projectTypeId)} width={500} height={id === 8 ? 650 : 350} />
       </TouchableOpacity>
     </Card>
   );
