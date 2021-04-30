1dcbe1b17c0a1b66ba533b52a8507c55efc3cf8a
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2021-04-30 11:39:53 +0200

    New translations

--- app/helpers/pre_login_translations.js
+++ app/helpers/pre_login_translations.js
@@ -6,11 +6,6 @@
 const preLoginTranslations = {
   "fr": [
     {
-      "id": 34,
-      "key": "loading",
-      "value": "Chargement..."
-    },
-    {
       "id": 3,
       "key": "login.placeholder",
       "value": "par ex. 0123456789"
@@ -21,14 +16,14 @@ const preLoginTranslations = {
       "value": "Erreur réseau"
     },
     {
+      "id": 34,
+      "key": "loading",
+      "value": "Chargement..."
+    },
+    {
       "id": 43,
       "key": "error.details",
       "value": "Détails de l'erreur:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Problème: le numéro de téléphone n'est pas reconnu."
     },
     {
       "id": 1,
@@ -46,252 +41,64 @@ const preLoginTranslations = {
       "value": "Désolé, il semble y avoir un problème avec nos ordinateurs. Veuillez réessayer plus tard."
     },
     {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Problème: le numéro de téléphone n'est pas reconnu."
+    },
+    {
       "id": 42,
       "key": "error.unknown.body",
       "value": "Désolé, il semble y avoir un problème. Si cela continue, essayez de réinstaller l'application."
     }
   ],
-  "ta": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "ஏற்றம்..."
-    },
-    {
-      "id": 3,
-      "key": "login.placeholder",
-      "value": "உ-ம்  0712345678"
-    },
-    {
-      "id": 39,
-      "key": "error.network.title",
-      "value": "வலையமைப்புத்தவறு"
-    },
-    {
-      "id": 43,
-      "key": "error.details",
-      "value": "பிழை விபரங்கள்"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "உள்நுழைவு தோல்வி, அங்கீகரிக்கப்படாத கைபேசி இலக்கம்."
-    },
-    {
-      "id": 1,
-      "key": "login.prompt",
-      "value": "உங்கள் கைபேசி இலக்கம் என்ன?"
-    },
-    {
-      "id": 41,
-      "key": "error.unknown.title",
-      "value": "எதிர்பாரத தவறு"
-    },
-    {
-      "id": 40,
-      "key": "error.network.body",
-      "value": "மன்னிக்கவும், எங்கள் கணிணிகளில் தவறு இருப்பதாகத் தெரிகிறது. தயவு செய்து சிறிது நேரத்தின் பின்னர் முயற்சி செய்யவும்."
-    },
-    {
-      "id": 42,
-      "key": "error.unknown.body",
-      "value": "மன்னிக்கவும், ஒரு  சிக்கல் இருப்பதாகத் தெரிகிறது.இவ்வாறு தொடர்ந்தால் செயலியை மீள றிறுவ முயற்சி செய்யவும்."
-    }
-  ],
-  "rw": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "Biracyaza…"
-    },
-    {
-      "id": 3,
-      "key": "login.placeholder",
-      "value": "Urugero: 07123456678"
-    },
-    {
-      "id": 39,
-      "key": "error.network.title",
-      "value": "Akabazo kuri rezo"
-    },
-    {
-      "id": 43,
-      "key": "error.details",
-      "value": "Akabazo kadasanzwe:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Kwinjira byanze. Nimero ya terefoni itazwi"
-    },
-    {
-      "id": 1,
-      "key": "login.prompt",
-      "value": "Nimero ya telefoni ukoresha ni iyihe?"
-    },
-    {
-      "id": 41,
-      "key": "error.unknown.title",
-      "value": "Akabazo katitezwe"
-    },
-    {
-      "id": 40,
-      "key": "error.network.body",
-      "value": "Ihanga, hashobora kuba hari ikibazo muri mudasobwa zacu. Nyabuneka, ongera ugerageze."
-    },
-    {
-      "id": 42,
-      "key": "error.unknown.body",
-      "value": "Ihangane, birashoboka ko hari ikibazo. Nibikomeza, gerageza wongere ushyiremo iyi apu."
-    }
-  ],
-  "en": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "Loading..."
-    },
-    {
-      "id": 3,
-      "key": "login.placeholder",
-      "value": "e.g. 0123456789"
-    },
-    {
-      "id": 39,
-      "key": "error.network.title",
-      "value": "Network error"
-    },
-    {
-      "id": 43,
-      "key": "error.details",
-      "value": "Error details:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Login failed. Unrecognised mobile number."
-    },
-    {
-      "id": 1,
-      "key": "login.prompt",
-      "value": "What’s your mobile number?"
-    },
-    {
-      "id": 41,
-      "key": "error.unknown.title",
-      "value": "Unexpected error"
-    },
-    {
-      "id": 40,
-      "key": "error.network.body",
-      "value": "Sorry, there seems to be a problem with our computers. Please try again later."
-    },
-    {
-      "id": 42,
-      "key": "error.unknown.body",
-      "value": "Sorry, there seems to be a problem. If it keeps happening, try reinstalling the app."
-    }
-  ],
-  "mg": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "Andalam-pamitana"
-    },
-    {
-      "id": 3,
-      "key": "login.placeholder",
-      "value": "Oh: 0123456789"
-    },
-    {
-      "id": 39,
-      "key": "error.network.title",
-      "value": "Misy olana ny tambazotram-pifandraisana"
-    },
-    {
-      "id": 43,
-      "key": "error.details",
-      "value": "Pitsopitsony amin'ny fahadisoana:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Tsy nahomby ny fidirana. Misy diso ny laharana finday."
-    },
-    {
-      "id": 1,
-      "key": "login.prompt",
-      "value": "Inona ny laharana finday anao?"
-    },
-    {
-      "id": 41,
-      "key": "error.unknown.title",
-      "value": "Fahadisoana tsy nampoizina"
-    },
-    {
-      "id": 40,
-      "key": "error.network.body",
-      "value": "Mialatsiny, ohatran'ny misy olana ny solosaina. Avereno afaka fotoana fohy"
-    },
-    {
-      "id": 42,
-      "key": "error.unknown.body",
-      "value": "Mialatsiny, ohatran'ny misy olana. Raha mbola mitohy dia avereno ampidirina ilay rindrambaiko."
-    }
-  ],
   "laj": [
     {
-      "id": 34,
-      "key": "loading",
-      "value": "Loading..."
-    },
-    {
       "id": 3,
       "key": "login.placeholder",
-      "value": "e.g. 0123456789"
+      "value": "Apor me nama cim 0716787679"
     },
     {
       "id": 39,
       "key": "error.network.title",
-      "value": "Network error"
+      "value": "Peko me netwak"
+    },
+    {
+      "id": 34,
+      "key": "loading",
+      "value": "Kur tye ayikere"
     },
     {
       "id": 43,
       "key": "error.details",
-      "value": "Error details:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Login failed. Unrecognised mobile number."
+      "value": "Anyut me jami mogo orocere:"
     },
     {
       "id": 1,
       "key": "login.prompt",
-      "value": "Nama cim ni tye adi?"
+      "value": "Nama cimi tye adi?"
     },
     {
       "id": 41,
       "key": "error.unknown.title",
-      "value": "Unexpected error"
+      "value": "Gin orocere ame timere ape iyikere iye"
     },
     {
       "id": 40,
       "key": "error.network.body",
-      "value": "Sorry, there seems to be a problem with our computers. Please try again later."
+      "value": "Timwa kica, peko moro tye I Kompiuta wa ni. Kong dok item  imede itemo."
+    },
+    {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Nama cim pe odonyo. Nama cim pe tye angeye."
     },
     {
       "id": 42,
       "key": "error.unknown.body",
-      "value": "Sorry, there seems to be a problem. If it keeps happening, try reinstalling the app."
+      "value": "Tim kica, ginoro tye orocere. Ka omede kede timere bed temo me cako keto puruguram."
     }
   ],
   "ro": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "Se încarcă"
-    },
     {
       "id": 3,
       "key": "login.placeholder",
@@ -303,14 +110,14 @@ const preLoginTranslations = {
       "value": "Eroare de rețea"
     },
     {
+      "id": 34,
+      "key": "loading",
+      "value": "Se încarcă"
+    },
+    {
       "id": 43,
       "key": "error.details",
       "value": "Detalii despre eroare:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Autentificare esuata. Număr mobil necunoscut."
     },
     {
       "id": 1,
@@ -328,17 +135,17 @@ const preLoginTranslations = {
       "value": "Ne pare rău, se pare că există o problemă cu calculatoarele noastre. Vă rugăm să încercați din nou mai târziu."
     },
     {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Autentificare esuata. Număr mobil necunoscut."
+    },
+    {
       "id": 42,
       "key": "error.unknown.body",
       "value": "Ne pare rău, se pare că există o problemă. Dacă continuă să se întâmple, încercați să reinstalați aplicația."
     }
   ],
   "es": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "Cargando..."
-    },
     {
       "id": 3,
       "key": "login.placeholder",
@@ -350,14 +157,14 @@ const preLoginTranslations = {
       "value": "Error de red"
     },
     {
+      "id": 34,
+      "key": "loading",
+      "value": "Cargando..."
+    },
+    {
       "id": 43,
       "key": "error.details",
       "value": "Error de detalles:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Error de inicio de sesion. Número de móvil no reconocido."
     },
     {
       "id": 1,
@@ -375,17 +182,158 @@ const preLoginTranslations = {
       "value": "Lo sentimos, parece haber un problema con nuestras computadoras. Por favor, inténtelo de nuevo más tarde."
     },
     {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Error de inicio de sesion. Número de móvil no reconocido."
+    },
+    {
       "id": 42,
       "key": "error.unknown.body",
       "value": "Lo siento, parece que hay un problema. Si sigue sucediendo, intente reinstalar la aplicación."
     }
   ],
-  "tet": [
+  "ta": [
+    {
+      "id": 3,
+      "key": "login.placeholder",
+      "value": "உ-ம்  0712345678"
+    },
+    {
+      "id": 39,
+      "key": "error.network.title",
+      "value": "வலையமைப்புத்தவறு"
+    },
     {
       "id": 34,
       "key": "loading",
-      "value": "Prosesu hela…"
+      "value": "ஏற்றம்..."
     },
+    {
+      "id": 43,
+      "key": "error.details",
+      "value": "பிழை விபரங்கள்"
+    },
+    {
+      "id": 1,
+      "key": "login.prompt",
+      "value": "உங்கள் கைபேசி இலக்கம் என்ன?"
+    },
+    {
+      "id": 41,
+      "key": "error.unknown.title",
+      "value": "எதிர்பாரத தவறு"
+    },
+    {
+      "id": 40,
+      "key": "error.network.body",
+      "value": "மன்னிக்கவும், எங்கள் கணிணிகளில் தவறு இருப்பதாகத் தெரிகிறது. தயவு செய்து சிறிது நேரத்தின் பின்னர் முயற்சி செய்யவும்."
+    },
+    {
+      "id": 2,
+      "key": "login.failed",
+      "value": "உள்நுழைவு தோல்வி, அங்கீகரிக்கப்படாத கைபேசி இலக்கம்."
+    },
+    {
+      "id": 42,
+      "key": "error.unknown.body",
+      "value": "மன்னிக்கவும், ஒரு  சிக்கல் இருப்பதாகத் தெரிகிறது.இவ்வாறு தொடர்ந்தால் செயலியை மீள றிறுவ முயற்சி செய்யவும்."
+    }
+  ],
+  "rw": [
+    {
+      "id": 3,
+      "key": "login.placeholder",
+      "value": "Urugero: 07123456678"
+    },
+    {
+      "id": 39,
+      "key": "error.network.title",
+      "value": "Akabazo kuri rezo"
+    },
+    {
+      "id": 34,
+      "key": "loading",
+      "value": "Biracyaza…"
+    },
+    {
+      "id": 43,
+      "key": "error.details",
+      "value": "Akabazo kadasanzwe:"
+    },
+    {
+      "id": 1,
+      "key": "login.prompt",
+      "value": "Nimero ya telefoni ukoresha ni iyihe?"
+    },
+    {
+      "id": 41,
+      "key": "error.unknown.title",
+      "value": "Akabazo katitezwe"
+    },
+    {
+      "id": 40,
+      "key": "error.network.body",
+      "value": "Ihanga, hashobora kuba hari ikibazo muri mudasobwa zacu. Nyabuneka, ongera ugerageze."
+    },
+    {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Kwinjira byanze. Nimero ya terefoni itazwi"
+    },
+    {
+      "id": 42,
+      "key": "error.unknown.body",
+      "value": "Ihangane, birashoboka ko hari ikibazo. Nibikomeza, gerageza wongere ushyiremo iyi apu."
+    }
+  ],
+  "en": [
+    {
+      "id": 3,
+      "key": "login.placeholder",
+      "value": "e.g. 0123456789"
+    },
+    {
+      "id": 39,
+      "key": "error.network.title",
+      "value": "Network error"
+    },
+    {
+      "id": 34,
+      "key": "loading",
+      "value": "Loading..."
+    },
+    {
+      "id": 43,
+      "key": "error.details",
+      "value": "Error details:"
+    },
+    {
+      "id": 1,
+      "key": "login.prompt",
+      "value": "What’s your mobile number?"
+    },
+    {
+      "id": 41,
+      "key": "error.unknown.title",
+      "value": "Unexpected error"
+    },
+    {
+      "id": 40,
+      "key": "error.network.body",
+      "value": "Sorry, there seems to be a problem with our computers. Please try again later."
+    },
+    {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Login failed. Unrecognised mobile number."
+    },
+    {
+      "id": 42,
+      "key": "error.unknown.body",
+      "value": "Sorry, there seems to be a problem. If it keeps happening, try reinstalling the app."
+    }
+  ],
+  "tet": [
     {
       "id": 3,
       "key": "login.placeholder",
@@ -397,14 +345,14 @@ const preLoginTranslations = {
       "value": "Rede faila/Rede ladun di'ak"
     },
     {
+      "id": 34,
+      "key": "loading",
+      "value": "Prosesu hela…"
+    },
+    {
       "id": 43,
       "key": "error.details",
       "value": "Erru detallu:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Prosesu faila. Numeru Telefone lalos."
     },
     {
       "id": 1,
@@ -422,17 +370,64 @@ const preLoginTranslations = {
       "value": "Deskulpa, hare'e ba sei iha probema ho ita nia komputadores. Favor koko fila fali."
     },
     {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Prosesu faila. Numeru Telefone lalos."
+    },
+    {
       "id": 42,
       "key": "error.unknown.body",
       "value": "Deskulpa,hare'e ba sei iha problema. Bainhira ida ne'e sei akontese, koko reinstala app."
     }
   ],
-  "pt": [
+  "mg": [
+    {
+      "id": 3,
+      "key": "login.placeholder",
+      "value": "Oh: 0123456789"
+    },
+    {
+      "id": 39,
+      "key": "error.network.title",
+      "value": "Misy olana ny tambazotram-pifandraisana"
+    },
     {
       "id": 34,
       "key": "loading",
-      "value": "Carregando..."
+      "value": "Andalam-pamitana"
     },
+    {
+      "id": 43,
+      "key": "error.details",
+      "value": "Pitsopitsony amin'ny fahadisoana:"
+    },
+    {
+      "id": 1,
+      "key": "login.prompt",
+      "value": "Inona ny laharana finday anao?"
+    },
+    {
+      "id": 41,
+      "key": "error.unknown.title",
+      "value": "Fahadisoana tsy nampoizina"
+    },
+    {
+      "id": 40,
+      "key": "error.network.body",
+      "value": "Mialatsiny, ohatran'ny misy olana ny solosaina. Avereno afaka fotoana fohy"
+    },
+    {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Tsy nahomby ny fidirana. Misy diso ny laharana finday."
+    },
+    {
+      "id": 42,
+      "key": "error.unknown.body",
+      "value": "Mialatsiny, ohatran'ny misy olana. Raha mbola mitohy dia avereno ampidirina ilay rindrambaiko."
+    }
+  ],
+  "pt": [
     {
       "id": 3,
       "key": "login.placeholder",
@@ -444,14 +439,14 @@ const preLoginTranslations = {
       "value": "Erro de rede"
     },
     {
+      "id": 34,
+      "key": "loading",
+      "value": "Carregando..."
+    },
+    {
       "id": 43,
       "key": "error.details",
       "value": "Detalhes do erro:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Falha no login. Número de celular desconhecido"
     },
     {
       "id": 1,
@@ -469,17 +464,17 @@ const preLoginTranslations = {
       "value": "Desculpe, parece que há um problema com nossos computadores. Por favor, tente novamente mais tarde."
     },
     {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Falha no login. Número de celular desconhecido"
+    },
+    {
       "id": 42,
       "key": "error.unknown.body",
       "value": "Desculpe, parece que há um problema. Se continuar acontecendo, tente reinstalar o aplicativo."
     }
   ],
   "am": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "እየጫነ"
-    },
     {
       "id": 3,
       "key": "login.placeholder",
@@ -491,14 +486,14 @@ const preLoginTranslations = {
       "value": "የኔትዎርክ ስህተት"
     },
     {
+      "id": 34,
+      "key": "loading",
+      "value": "እየጫነ"
+    },
+    {
       "id": 43,
       "key": "error.details",
       "value": "የስህተት ዝርዝር"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "የመግባት ተግባር አልተሳካም፡፡ የማይታወቅ ስልክ ቁጥር"
     },
     {
       "id": 1,
@@ -516,17 +511,17 @@ const preLoginTranslations = {
       "value": "ይቅርታ በእኛ ኮምፕዩተር ላይ ችግር ያለ ይመስላል፡፡ እባክዎትን ትንሽ ቆይተው ይሞክሩ"
     },
     {
+      "id": 2,
+      "key": "login.failed",
+      "value": "የመግባት ተግባር አልተሳካም፡፡ የማይታወቅ ስልክ ቁጥር"
+    },
+    {
       "id": 42,
       "key": "error.unknown.body",
       "value": "ይቅርታ፣ ችግር ያለ ይመስላል፡፡ ችግሩም ከቀጠለ አፕሊኬሽኑን እንደገና ለማስነሳት ይሞክሩ"
     }
   ],
   "sw": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "Inapakia ..."
-    },
     {
       "id": 3,
       "key": "login.placeholder",
@@ -538,14 +533,14 @@ const preLoginTranslations = {
       "value": "Tatizo la mtandao"
     },
     {
+      "id": 34,
+      "key": "loading",
+      "value": "Inapakia ..."
+    },
+    {
       "id": 43,
       "key": "error.details",
       "value": "Maelezo ya itilafu:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Imeshindikana kuingia. Nambari ya simu aiyasajiliwa kwenye mfumo"
     },
     {
       "id": 1,
@@ -563,17 +558,17 @@ const preLoginTranslations = {
       "value": "Samahani, inaonekana kuna itilafu na kompyuta zetu. Tafadhali jaribu tena baadae."
     },
     {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Imeshindikana kuingia. Nambari ya simu aiyasajiliwa kwenye mfumo"
+    },
+    {
       "id": 42,
       "key": "error.unknown.body",
       "value": "Samahani, inaonekana kuna itilafu. Ikiendelea kutokea, jaribu kupakua tena programu."
     }
   ],
   "id": [
-    {
-      "id": 34,
-      "key": "loading",
-      "value": "Memuat"
-    },
     {
       "id": 3,
       "key": "login.placeholder",
@@ -585,14 +580,14 @@ const preLoginTranslations = {
       "value": "Terjadi kesalahan jaringan"
     },
     {
+      "id": 34,
+      "key": "loading",
+      "value": "Memuat"
+    },
+    {
       "id": 43,
       "key": "error.details",
       "value": "rincian kesalahan:"
-    },
-    {
-      "id": 2,
-      "key": "login.failed",
-      "value": "Gagal masuk. \nNomor ponsel tidak dikenali"
     },
     {
       "id": 1,
@@ -608,6 +603,11 @@ const preLoginTranslations = {
       "id": 40,
       "key": "error.network.body",
       "value": "Maaf, sepertinya ada masalah dengan komputer kami. Silakan coba lagi nanti."
+    },
+    {
+      "id": 2,
+      "key": "login.failed",
+      "value": "Gagal masuk. \nNomor ponsel tidak dikenali"
     },
     {
       "id": 42,
